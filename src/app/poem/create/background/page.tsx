5b0682cc032bc82d41067fcb9f3798df77bcb78c
turtledev <turtledev65@gmail.com>
2024-11-07 15:30:36 +0200

    Also preview the poem

--- src/app/poem/create/background/page.tsx
+++ src/app/poem/create/background/page.tsx
@@ -6,12 +6,16 @@ import AppearanceProvider, {
 import Sidebar from "./_components/sidebar";
 import { useContext } from "react";
 import BaseBackground from "../../_components/background";
+import BasePoem from "../../_components/poem";
 
 const BackgroundPage = () => {
   return (
     <main>
       <AppearanceProvider>
         <Background />
+        <div className="flex">
+          <Poem />
+        </div>
         <Sidebar />
       </AppearanceProvider>
     </main>
@@ -22,4 +26,16 @@ export default BackgroundPage;
 const Background = () => {
   const { appearance } = useContext(AppearanceContext);
   return <BaseBackground appearance={appearance.background} />;
+};
+
+const Poem = () => {
+  const { appearance } = useContext(AppearanceContext);
+
+  return (
+    <BasePoem
+      title="Title"
+      text="here is some text"
+      appearance={appearance.foreground}
+    />
+  );
 };
