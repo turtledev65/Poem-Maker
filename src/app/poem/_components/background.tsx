554d9285cb3ed355be14039e7f0a62418afbedad
turtledev <turtledev65@gmail.com>
2024-11-09 21:42:56 +0200

    Make the backgorund a server component

--- src/app/poem/_components/background.tsx
+++ src/app/poem/_components/background.tsx
@@ -1,43 +1,42 @@
-"use client";
-
-import { CSSProperties, useMemo } from "react";
+import { CSSProperties } from "react";
 import { Background as BackgroundType } from "@/types";
 
 const Background = ({ appearance }: { appearance: BackgroundType }) => {
-  const styles = useMemo<CSSProperties>(() => {
-    switch (appearance.type) {
-      case "color":
-        return { background: appearance.value };
-      case "linear-gradient":
-        if (appearance.values.length === 0) {
-          return { background: "#ffffff" };
-        } else if (appearance.values.length == 1) {
-          return { background: appearance.values[0].value };
-        }
-
-        return {
-          background: `linear-gradient(${appearance.angle}deg, ${getColors(appearance)})`,
-        };
-      case "radial-gradient":
-        if (appearance.values.length === 0) {
-          return { background: "#ffffff" };
-        } else if (appearance.values.length == 1) {
-          return { background: appearance.values[0].value };
-        }
-
-        return {
-          background: `radial-gradient(circle, ${getColors(appearance)})`,
-        };
-      default:
-        return {};
-    }
-  }, [appearance]);
-
+  const styles = getStyles(appearance);
   return (
     <div className="poem-background absolute inset-0 -z-10" style={styles} />
   );
 };
 export default Background;
+
+function getStyles(appearance: BackgroundType): CSSProperties {
+  switch (appearance.type) {
+    case "color":
+      return { background: appearance.value };
+    case "linear-gradient":
+      if (appearance.values.length === 0) {
+        return { background: "#ffffff" };
+      } else if (appearance.values.length == 1) {
+        return { background: appearance.values[0].value };
+      }
+
+      return {
+        background: `linear-gradient(${appearance.angle}deg, ${getColors(appearance)})`,
+      };
+    case "radial-gradient":
+      if (appearance.values.length === 0) {
+        return { background: "#ffffff" };
+      } else if (appearance.values.length == 1) {
+        return { background: appearance.values[0].value };
+      }
+
+      return {
+        background: `radial-gradient(circle, ${getColors(appearance)})`,
+      };
+    default:
+      return {};
+  }
+}
 
 function getColors(settings: BackgroundType) {
   if (settings.type === "color") return null;
