3dc38f1d3663ead517e466ace62b315839ee3d96
turtledev <turtledev65@gmail.com>
2024-11-09 20:00:41 +0200

    Add an id column to the poem table

---
+++ drizzle/0002_curved_smasher.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "poem" ADD COLUMN "id" serial PRIMARY KEY NOT NULL;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,8 @@
 import { Appearance } from "@/types";
-import { jsonb, pgTable, text } from "drizzle-orm/pg-core";
+import { jsonb, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const poemTable = pgTable("poem", {
+  id: serial("id").primaryKey(),
   title: text("title").notNull(),
   text: text("text").notNull(),
   appearance: jsonb().$type<Appearance>().notNull(),
