67430f3e56b5c850ea686e29087d5fb490ecb5a0
turtledev <turtledev65@gmail.com>
2024-11-13 17:30:13 +0200

    Turn the explore page into a client component and add loading and error states

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,17 +1,31 @@
-import { db } from "@/db";
+"use client";
+import { poemTable } from "@/db/schema";
 import { replaceWithBr } from "@/util/text";
-import { Metadata } from "next";
+import { InferSelectModel } from "drizzle-orm";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { getAllPoems } from "../poem/create/actions";
+import { AiOutlineLoading } from "react-icons/ai";
 
-export const metadata: Metadata = {
-  title: "Explore",
-};
+const ExplorePage = () => {
+  const [poems, setPoems] = useState<InferSelectModel<typeof poemTable>[]>();
+  const [state, setState] = useState<"loading" | "error" | "finished">(
+    "loading",
+  );
 
-const ExplorePage = async () => {
-  const poems = await db.query.poemTable.findMany({ limit: 20 });
+  useEffect(() => {
+    getAllPoems()
+      .then(res => {
+        setPoems(res);
+        setState("finished");
+      })
+      .catch(_ => {
+        setState("error");
+      });
+  }, [setPoems, setState]);
 
   return (
-    <main className="flex h-full flex-1 flex-col justify-between gap-4 px-6 py-4 md:flex-row">
+    <main className="flex h-full flex-1 flex-col gap-4 px-6 py-4 md:flex-row">
       <div className="rounded-lg bg-gray-100 px-4 py-6 dark:bg-neutral-900">
         <div className="sticky top-20 flex flex-col items-center">
           <h1 className="mx-auto mb-6 inline-block w-min text-center text-6xl font-bold dark:text-gray-50">
@@ -22,12 +36,21 @@ const ExplorePage = async () => {
           </h1>
           <input
             placeholder="Search"
-            className="rounded-full border-2 border-gray-600 px-3 py-2 text-lg outline-none dark:border-neutral-500 dark:bg-neutral-700"
+            className="rounded-full border-2 border-gray-600 px-3 py-2 text-lg outline-none dark:border-neutral-500 dark:bg-neutral-700 dark:text-white"
           />
         </div>
       </div>
+      {state != "finished" && (
+        <div className="flex flex-1 items-center justify-center">
+          {state === "loading" ? (
+            <AiOutlineLoading className="animate-spin text-3xl dark:text-white" />
+          ) : (
+            <p className="text-lg text-red-500">An unexpected error occurred</p>
+          )}
+        </div>
+      )}
       <div className="grid w-full grid-cols-1 gap-4 px-8 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-        {poems.map(poem => (
+        {poems?.map(poem => (
           <PoemPreview
             title={poem.title}
             text={poem.text}
@@ -50,7 +73,7 @@ const PoemPreview = ({ title, text, id }: PoemPreviewProps) => {
   return (
     <Link
       href={`/poem/${id}`}
-      className="flex w-full flex-col items-center justify-center gap-10 rounded border-2 border-transparent bg-gray-50 p-2 text-center transition-all hover:border-gray-400 dark:bg-neutral-900 dark:text-gray-50 hover:dark:border-neutral-500"
+      className="flex w-full flex-col items-center justify-center gap-10 rounded border-2 border-transparent bg-gray-100 p-2 text-center transition-all hover:border-gray-400 dark:bg-neutral-900 dark:text-gray-50 hover:dark:border-neutral-500"
     >
       <p
         dangerouslySetInnerHTML={{
--- src/app/poem/create/actions.ts
+++ src/app/poem/create/actions.ts
@@ -4,6 +4,10 @@ import { db } from "@/db";
 import { poemTable } from "@/db/schema";
 import { Poem } from "@/types";
 
+export async function getAllPoems() {
+  return await db.query.poemTable.findMany();
+}
+
 export async function savePoem(newPoem: Poem) {
   const [poem] = await db
     .insert(poemTable)
